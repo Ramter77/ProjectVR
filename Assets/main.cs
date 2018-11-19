3873968f9b9e0d3edfbc34e4be0ee0bf782d41c0
Ramter77 <34648210+Ramter77@users.noreply.github.com>
2018-11-19 17:52:43 +0100

    init
    init

--- Assets/main.cs
+++ Assets/main.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class main.js : MonoBehaviour
+public class main : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
