468df469dba85775900b5da75334b5fcf5f7935b
Ramter77 <34648210+Ramter77@users.noreply.github.com>
2018-11-19 18:03:50 +0100

    menu test

---
+++ Assets/Scenes/navigateToScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class navigateToScene : MonoBehaviour
+{
+
+    //public string level;
+    public void loadScene(string level) {
+        SceneManager.LoadScene(level);
+    }
+}
