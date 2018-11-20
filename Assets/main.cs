89b38b8d4f92d8e10b198dbbda5fc2cef02b6244
Ramter77 <34648210+Ramter77@users.noreply.github.com>
2018-11-20 12:09:55 +0100

    comment

--- Assets/main.cs
+++ Assets/main.cs
@@ -3,13 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class main : MonoBehaviour
-
-
 {
-
     public int test;
 
-    
+    //test it you twat
     // Start is called before the first frame update
     void Start()
     {
