8a6983c15b7e34e9ffc62fd18befae4eb6438ee7
Diego Blank <diegobrunoblank@gmail.com>
2018-11-20 11:24:29 +0100

    Test

--- Assets/main.cs
+++ Assets/main.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class main : MonoBehaviour
+
+
 {
+
+    public int test;
+
+    
     // Start is called before the first frame update
     void Start()
     {
